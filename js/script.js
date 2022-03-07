8c03be4086ff01467c7828f06685805e77d31429
kris <cmuchiri810@gmail.com>
2022-03-07 13:53:46 +0300

    modified html/script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-//Back-end logic:
-let akan=function(year, month, date, gender) {
-    let maleNames=["Kwasi","Kudwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    let dateEntered = newDate(year, --month, date);
-    if (gender == "Male"){
-    return date && maleNames[date.getDay()];
-    }
-    else if (gender == "Female"){
-    return date && femaleNames[date.getDay()];
-    }
-    }
-    //Front-end logic:
+// //Back-end logic:
+// let akan=function(year, month, date, gender) {
+//     let maleNames=["Kwasi","Kudwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+//     let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+//     let dateEntered = newDate(year, --month, date);
+//     if (gender == "Male"){
+//     return date && maleNames[date.getDay()];
+//     }
+//     else if (gender == "Female"){
+//     return date && femaleNames[date.getDay()];
+//     }
+//     }
+//     //Front-end logic:
     
 
+// function submitForm(); {
+//     event.preventDefault();
+// }
