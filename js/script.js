4d87000f9057c993f2cfdde0a0e637cc05091ab5
kris <cmuchiri810@gmail.com>
2022-03-07 09:43:37 +0300

    modified back-end logic

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 let akan=function(year, month, date, gender) {
     let maleNames=["Kwasi","Kudwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    let date = newDate(year, --month, date);
-    if (gender=== "Male"){
+    let dateEntered = newDate(year, --month, date);
+    if (gender == "Male"){
     return date && maleNames[date.getDay()];
     }
-    else{
+    else if (gender == "Female"){
     return date && femaleNames[date.getDay()];
     }
     }
     //Front-end logic:
+    
+
