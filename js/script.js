37df7d7a83a61f6d3f2f14e0201cdd6e17a12a33
kris <cmuchiri810@gmail.com>
2022-03-07 22:44:44 +0300

    script.js modified

--- js/script.js
+++ js/script.js
@@ -1,32 +1,16 @@
-// //Back-end logic:
-// let akan=function(year, month, date, gender) {
-//    let maleNames=["Kwasi","Kudwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-//    let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-//     let dateEntered = newDate(year, --month, date);
-//     if (gender == "Male"){
-//     return date && maleNames[date.getDay()];
-//     }
-//     else if (gender == "Female"){
-//     return date && femaleNames[date.getDay()];
-//     }
-//     }
-//     //Front-end logic:
-    
 "use strict";
 
 let nameMenu = document.querySelector("#akan");
 
 let dateEntered = new Date(document.querySelector("#date").value);
-let date=document.querySelector("#date")
 
-// let button = document.querySelector("#submit");
+let date=document.querySelector("#date")
 
 let maleNames=["Kwasi","Kudwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
 let femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 let finalMenu = dateEntered.getDate();
-// alert(finalMenu);
 
 function submitForm() {
     event.preventDefault();
@@ -35,21 +19,23 @@ function submitForm() {
     console.log(maleName.checked);
 
     let femaleName=document.querySelector("#thefemale")
-    console.log(femaleName.checked);
 
-    console.log(date.value);
-    console.log()
+    console.log(femaleName.checked);
 
     let d=new Date(date.value)
     console.log(d.getDay());
 
+    console.log(date.value);
+    console.log()
+
+
     if (maleName.checked == true) {
-        nameMenu.innerText = "Your name is: "+ (maleNames[d.getDay()]);
+        nameMenu.innerText= "Your name is: "+ (maleNames[d.getDay()]);
         alert("Your name is: "+ (maleNames[dateEntered]))
     }
     else if (femaleName.checked == true) {
-            nameMenu.innerText = "Your name is: "+ (maleNames[d.getDay()]);
-            alert("Your name is: "+ (maleNames[dateEntered]))
+            nameMenu.innerText = "Your name is: "+ (femaleNames[d.getDay()]);
+            alert("Your name is: "+ (femaleNames[dateEntered]))
 
     }
    
